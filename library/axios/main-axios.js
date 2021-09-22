adef699495a1e1a46165452c8f02bf8120a8252a
victor-tungui <capishy@gmail.com>
2021-09-21 20:02:51 -0500

    update nextjs version
    fix the css issue with the new version.
    change the URL to be coming from env.local

--- library/axios/main-axios.js
+++ library/axios/main-axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const mainAxios = axios.create({
-  baseURL: 'http://localhost:54637/api/',
+  baseURL: process.env.NEXT_PUBLIC_BASE_API,
   headers: {
     Accept: '*/*',
     'Content-Type': 'application/json',
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+//import '../styles/base.css';
 import { Provider } from 'next-auth/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import store from '../store/store';
 
 import Layout from '../components/ui/layout/layout';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <ReduxProvider store={store}>
       <Provider session={pageProps.session}>
@@ -18,4 +18,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp;
+//export default MyApp;
